cbf6534e72448b646c3097b06a2511e659b1252f
leon.dahmer <leon.dahmer@gmail.com>
2019-02-25 14:32:04 -0500

    Gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,19 @@
 
 orgtour = Tour.create(name: "Organizator Erste Schritte", description: "", image_url: "")
 
-org1 = Step.create(step: 1, tour_id: orgtour, head: "YooYuu kennenlernen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org1 = Step.create(step: 1, tour_id: 1, head: "YooYuu kennenlernen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
-org2 = Step.create(step: 2, tour_id: orgtour, head: "Aufbau der App", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org2 = Step.create(step: 2, tour_id: 1, head: "Aufbau der App", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
-org3 = Step.create(step: 3, tour_id: orgtour, head: "Weitere Funktionen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org3 = Step.create(step: 3, tour_id: 1, head: "Weitere Funktionen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
-org4 = Step.create(step: 4, tour_id: orgtour, head: "Orga-App & Web-App", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org4 = Step.create(step: 4, tour_id: 1, head: "Orga-App & Web-App", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
-org5 = Step.create(step: 5, tour_id: orgtour, head: "Kategorien & Gruppen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org5 = Step.create(step: 5, tour_id: 1, head: "Kategorien & Gruppen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
-org6 = Step.create(step: 6, tour_id: orgtour, head: "Nutzer anlegen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org6 = Step.create(step: 6, tour_id: 1, head: "Nutzer anlegen", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
-org7 = Step.create(step: 7, tour_id: orgtour, head: "Handy verloren, etc.", body: "", type: "", video_url: "", image_url: "", est_time: 3);
+org7 = Step.create(step: 7, tour_id: 1, head: "Handy verloren, etc.", body: "", type: "", video_url: "", image_url: "", est_time: 3);
 
 # Angestellte Erste Schritte
 
